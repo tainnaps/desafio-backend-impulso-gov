458b1d967be2926b1502bad4f10de74b43a6de98
Taina Santana <tainnaps@gmail.com>
2023-01-06 19:30:07 -0300

    feat: add establishment dict type

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
+from typing import TypedDict
 
 from src.model import EstablishmentModel
+
+
+class Establishment(TypedDict):
+    cnes: str
+    nome: str
+    latitude: float
+    longitude: float
 
 
 @dataclass

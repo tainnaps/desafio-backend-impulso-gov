b2841cad25af9a8bbd3faaf2403de67e39bd90c7
Taina Santana <tainnaps@gmail.com>
2023-01-06 20:19:57 -0300

    test: integration add get_all_by_city_id_success test case

--- tests/test_get_all_by_city_id.py
+++ tests/test_get_all_by_city_id.py
@@ -1,6 +1,22 @@
+import json
+
+import pytest
 from fastapi.testclient import TestClient
 
 from src.main import app
 
 ENDPOINT = "/listarEstabelecimentosPorMunicipio"
 client = TestClient(app)
+
+
+@pytest.mark.parametrize("city_id", [316292, 320140, "316292", "320140"])
+def test_get_all_by_city_id_success(city_id):
+    response = client.get(f"{ENDPOINT}/{city_id}")
+    assert response.status_code == 200
+
+    with open(f"tests/mocks/get_all_by_city_id_{city_id}.json", "r") as file:
+        content = file.read()
+        mocked_json = json.loads(content)
+    assert response.json() == mocked_json
+
+

7f602932cd9f6e92674e31cc09a0c0acc32033ae
Taina Santana <tainnaps@gmail.com>
2023-01-06 20:22:34 -0300

    test: integration add get_by_cnes_id_success test case

---
+++ tests/test_get_by_cnes_id.py
@@ -0,0 +1,21 @@
+import json
+
+import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+ENDPOINT = "/obterEstabelecimentoPorId"
+client = TestClient(app)
+
+
+@pytest.mark.parametrize("cnes_id", [2187396, 9729135, "2187396", "9729135"])
+def test_get_by_cnes_id_success(cnes_id):
+    response = client.get(f"{ENDPOINT}/{cnes_id}")
+    assert response.status_code == 200
+
+    with open(f"tests/mocks/get_by_cnes_id_{cnes_id}.json", "r") as file:
+        content = file.read()
+        mocked_json = json.loads(content)
+    assert response.json() == mocked_json
+
